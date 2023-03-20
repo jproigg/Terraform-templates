a58880c7773862dce51315b6803de66448f2f324
jproigg <80080878+jproigg@users.noreply.github.com>
2023-03-19 21:36:16 -0600

    one ec2 test

--- terraform-aws-EC2/main.tf
+++ terraform-aws-EC2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0f3c9c466bb525749"
   instance_type = "t2.micro"
-  count = 4
+  count = 1
 
   tags = var.ec2-tags
 }
--- terraform-aws-EC2/variables.tfvars
+++ terraform-aws-EC2/variables.tfvars
@@ -1,3 +1,4 @@
 ec2-tags = {
     Name = "Ec2"
   }
+
